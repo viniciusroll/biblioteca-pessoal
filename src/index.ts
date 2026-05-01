407a1eb8d91450e217e0510c93f8d64edd7a11e3
viniciusroll <viniciusrodrigo879@gmail.com>
2026-05-01 11:30:11 -0300

    Implementa cadastro, remocao de livros e correcao de sintaxe

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const lido: boolean[] = [true, true, false, true, false];
 const avaliacoes: number[] = [5, 4, 0, 5, 0];
 
 function exibirBiblioteca(): void{
-    titulos.forEach((nomes, indice) => {
+    titulos.forEach((titulo, indice) => {
         const numero = indice + 1
 
         let status = "";
@@ -17,8 +17,41 @@ function exibirBiblioteca(): void{
             status = "PENDENTE";
         }
 
-        console.log (`${numero}. "${titulos}" (${anos[indice]}) - ${autores[indice]} - ${paginas[indice]} pag - ${status}`);
+        console.log (`${numero}. "${titulo}" (${anos[indice]}) - ${autores[indice]} - ${paginas[indice]} pag - ${status}`);
     })
 }
 
+exibirBiblioteca();
+
+function adicionarLivro(titulo: string, autor: string, ano: number, pagina: number): void{
+    if (ano > 0 && pagina > 0){
+        titulos.push(titulo);
+        autores.push(autor);
+        anos.push(ano);
+        paginas.push(pagina);
+        lido.push(false);
+        avaliacoes.push(0);
+    }
+    else{
+        console.log("Dados invalidos: ano e paginas devem ser maiores que 0");
+    }
+}
+
+function removerLivro(numeroLivro: number): void{
+    const indice = numeroLivro - 1;
+    if (indice < 0 || indice >= titulos.length){
+        console.log("Indice invalido");
+        return;
+    }
+    titulos.splice(indice, 1);
+    autores.splice(indice, 1);
+    anos.splice(indice, 1);
+    paginas.splice(indice, 1);
+    lido.splice(indice, 1);
+    avaliacoes.splice(indice, 1);
+}
+
+adicionarLivro("Senhor dos Aneis", "Tolkien", 1954, 1200);
+adicionarLivro("Gatos Guerreios", "Erin", 2010, 317);
+removerLivro(0);
 exibirBiblioteca();
